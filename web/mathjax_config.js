91cc464b00574061b0527d8a55ae3d297f1362ac
Magne Halvorsen <mh95244719@gmail.com>
2021-06-04 09:14:35 +0200

    Upgraded MathJax to 3

---
+++ web/mathjax_config.js
@@ -0,0 +1,12 @@
+MathJax = {
+  options: {
+    enableMenu: false,
+  },
+  loader: { load: ["[tex]/physics"] },
+  tex: {
+    // packages: [],
+    macros: {
+      R: "\\mathbb{R}",
+    },
+  },
+};
